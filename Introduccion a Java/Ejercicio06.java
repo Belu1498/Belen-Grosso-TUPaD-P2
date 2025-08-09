7e43eba4cc9a7550fbdad457c6d498e678d0231e
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-08-09 18:40:23 -0300

    Create Ejercicio06.java

---
+++ Introduccion a Java/Ejercicio06.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author belen
+ */
+public class Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            
+            String nombre, direccion;
+            int edad;
+            
+            nombre = "Juan Perez";
+            edad = 30;
+            direccion = " \" Calle Falsa 123 \"";  
+            
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Edad: " + edad + " anios");
+              System.out.println("Direccion: " + direccion);
+            
+    }
+    
+}
