7768c9b76cf9621a42f9bd171efb450d6abea630
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-08-09 18:39:17 -0300

    Create Ejercicio03.java

---
+++ Introduccion a Java/Ejercicio03.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author belen
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+          String nombre;
+          int edad;
+          double altura;
+          boolean estudiante;
+          
+          nombre = "Belen";
+          
+         edad = 26;
+         
+         altura = 1.80;
+         
+         estudiante = true;
+          
+          System.out.println(nombre);
+          System.out.println(edad);
+          System.out.println(altura);
+          System.out.println(estudiante);
+           
+    }
+    
+}
