9e87c673fe7d68c42eaf8dbb02fed2be2f9a2c48
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-20 12:33:46 -0300

    Add Cancion class with title and artist attributes

---
+++ UML Basico /Ejercicio11/Cancion.java
@@ -0,0 +1,22 @@
+public class Cancion {
+    private final String titulo;
+    private final Artista artista;
+
+    public Cancion(String titulo, Artista artista) {
+        this.titulo = titulo;
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Canción -- > Título=" + titulo + ", Artista=" + artista.getNombre() + "]";
+    }
+}
