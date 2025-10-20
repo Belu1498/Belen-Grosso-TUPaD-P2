7e0b201d13c494991f9b36153b9694d445ac6805
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-20 18:23:30 -0300

    Create Producto.java

---
+++ Colecciones/Ejercicio1/Producto.java
@@ -0,0 +1,44 @@
+public class Producto {
+    private String id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    private CategoriaProducto categoria;
+
+
+    public Producto(String id, String nombre, double precio, int cantidad, CategoriaProducto categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+    }
+
+
+    public Producto(String id, String nombre, double precio, CategoriaProducto categoria) {
+        this(id, nombre, precio, 0, categoria); // usa this para llamar al otro constructor
+    }
+
+    public void mostrarInfo() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto {" +
+                "ID='" + id + '\'' +
+                ", Nombre='" + nombre + '\'' +
+                ", Precio=$" + precio +
+                ", Cantidad=" + cantidad +
+                ", Categoría=" + categoria +
+                " (" + categoria.getDescripcion() + ")" +
+                '}';
+    }
+    
+    public String getId() { return id; }
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+    public int getCantidad() { return cantidad; }
+    public CategoriaProducto getCategoria() { return categoria; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+}
