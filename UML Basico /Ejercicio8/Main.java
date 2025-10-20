530c039a6e55f59757a13957a1f9b07cd7162a3a
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-20 11:16:26 -0300

    Update Main.java

--- UML Basico /Ejercicio8/Main.java
+++ UML Basico /Ejercicio8/Main.java
@@ -1,1 +1,14 @@
+public class Ejercicio8 {
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {       
+        Usuario usuario = new Usuario("Belen Grosso", "belen@gmail.com");
+        Documento documento = new Documento("Contrato Laboral", "Contenido confidencial", usuario, "A1B2C3D4", "20/10/2025");
+
+        System.out.println(documento);
+        System.out.println("Usuario asociado: " + documento.getFirma().getUsuario());
+    }
+    }
+    
