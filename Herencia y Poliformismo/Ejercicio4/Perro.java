c8664c5a4340ccdd446ddb5c54f66a50edbe7ef8
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-21 20:26:30 -0300

    Create Perro.java

---
+++ Herencia y Poliformismo/Ejercicio4/Perro.java
@@ -0,0 +1,14 @@
+package ejercicio4;
+
+
+public class Perro extends Animal {
+
+    public Perro(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(nombre + " dice: ¡Guau guau!");
+    }
+}
