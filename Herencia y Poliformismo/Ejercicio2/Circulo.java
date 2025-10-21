2d6c2b82ee9b1d61dfac4fb306739a2c4183ae4f
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-21 12:22:47 -0300

    Create Circulo.java

---
+++ Herencia y Poliformismo/Ejercicio2/Circulo.java
@@ -0,0 +1,17 @@
+public class Circulo extends Figura {
+    private final double radio;
+
+    public Circulo(double radio) {
+        super("Circulo");
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+}
