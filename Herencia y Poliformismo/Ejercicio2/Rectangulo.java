a6c6573b4dd88c0c0849c6cfa6fcdcad485c172d
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-21 12:23:09 -0300

    Create Rectangulo.java

---
+++ Herencia y Poliformismo/Ejercicio2/Rectangulo.java
@@ -0,0 +1,15 @@
+public class Rectangulo extends Figura {
+    private final double base;
+    private final double altura;
+
+    public Rectangulo(double base, double altura) {
+        super("Rectangulo");
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+}
