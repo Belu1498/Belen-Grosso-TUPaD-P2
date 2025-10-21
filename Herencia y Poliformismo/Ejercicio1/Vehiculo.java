d4d9a1b6dfea0e91160683557c9607bb7aca394e
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-21 12:14:45 -0300

    Create Vehiculo.java

---
+++ Herencia y Poliformismo/Ejercicio1/Vehiculo.java
@@ -0,0 +1,16 @@
+public class Vehiculo {
+    protected String marca;
+    protected String modelo;
+
+    // Constructor
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    // Método para mostrar información
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+    }
+}
