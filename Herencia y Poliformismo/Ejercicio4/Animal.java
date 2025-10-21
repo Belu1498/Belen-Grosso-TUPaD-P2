96d32c04db2d544a094bc647da886594d9f0894d
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-21 20:26:16 -0300

    Create Animal.java

---
+++ Herencia y Poliformismo/Ejercicio4/Animal.java
@@ -0,0 +1,20 @@
+package ejercicio4;
+
+
+public class Animal {
+    protected String nombre;
+
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Método que será sobrescrito
+    public void hacerSonido() {
+        System.out.println("El animal hace un sonido genérico.");
+    }
+
+    // Método común a todos
+    public void describirAnimal() {
+        System.out.println("Soy un animal llamado " + nombre + ".");
+    }
+}
