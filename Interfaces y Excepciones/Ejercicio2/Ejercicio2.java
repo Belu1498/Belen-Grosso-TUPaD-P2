7e120403d4712f60761ba22974fdef2e516941c8
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-23 20:15:41 -0300

    Update Ejercicio2.java

--- Interfaces y Excepciones/Ejercicio2/Ejercicio2.java
+++ Interfaces y Excepciones/Ejercicio2/Ejercicio2.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio2;
 
 
@@ -148,3 +144,4 @@ public class Ejercicio2{
         }
     }
 }
+
