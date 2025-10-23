b145d45241936fc507f768264fecd2289fd9ff67
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-23 12:02:42 -0300

    Create Cliente.java

---
+++ Interfaces y Excepciones/Ejercicio1/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente implements Notificable {
+    private final String nombre;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void notificar(String mensaje) {
+        System.out.println("Notificacion para " + nombre + ": " + mensaje);
+    }
+}
