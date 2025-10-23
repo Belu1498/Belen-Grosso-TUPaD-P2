c45e023e877afe9ce99c0339d99c1ae643a78aa1
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-23 20:13:05 -0300

    Add files via upload

---
+++ Interfaces y Excepciones/LecturaArchivo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author belen
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LecturaArchivo {
+    public static void main(String[] args) {
+        try {
+            File archivo = new File("datos.txt");
+            Scanner lector = new Scanner(archivo);
+
+            while (lector.hasNextLine()) {
+                String linea = lector.nextLine();
+                System.out.println(linea);
+            }
+
+            lector.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: El archivo no existe.");
+        }
+    }
+}
