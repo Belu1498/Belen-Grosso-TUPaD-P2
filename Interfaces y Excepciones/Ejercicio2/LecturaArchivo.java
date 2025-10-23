61e5f2d990d81a2b862eeb9c46f9650b5040d845
Belu Grosso <139245627+Belu1498@users.noreply.github.com>
2025-10-23 20:16:30 -0300

    Update LecturaArchivo.java

--- Interfaces y Excepciones/Ejercicio2/LecturaArchivo.java
+++ Interfaces y Excepciones/Ejercicio2/LecturaArchivo.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejercicio2;
 
-/**
- *
- * @author belen
- */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -30,3 +24,4 @@ public class LecturaArchivo {
         }
     }
 }
+
